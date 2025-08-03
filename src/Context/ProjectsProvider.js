22f3372aa5e527dcea6973a4ea4fbc2cb434da07
pikoseq9 <wekpat@wp.pl>
2025-08-03 10:59:23 +0200

    add private token for github APIs + add button transition styling

--- src/Context/ProjectsProvider.js
+++ src/Context/ProjectsProvider.js
@@ -5,13 +5,18 @@ export const ProjectsProvider = ({ children }) => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/users/pikoseq9/repos")
+    const token = process.env.REACT_APP_GITHUB_TOKEN;
+
+    fetch("https://api.github.com/users/pikoseq9/repos", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github+json",
+      },
+    })
       .then((result) => result.json())
       .then((data) => setProjects(data))
       .catch((err) => console.error(err));
   }, []);
-
-  console.log(projects);
 
   return (
     <ProjectsContext.Provider value={projects}>
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,36 @@
-import React from "react";
-import ProjectsContext from "../Context/ProjectsContext";
+import React, { useState, useEffect } from "react";
+import GitHubButton from "./GitHubButton";
 
 export default function ProjectCard({ project }) {
+  const [langs, setLangs] = useState([]);
+
+  useEffect(() => {
+    const token = process.env.REACT_APP_GITHUB_TOKEN;
+
+    fetch(project.languages_url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github+json",
+      },
+    })
+      .then((result) => result.json())
+      .then((data) => setLangs(Object.keys(data).map((key) => key)))
+      .catch((err) => console.error(err));
+  }, [project.languages_url]);
+
   return (
     <div className="project-card" key={project.id}>
-      <h1>{project.full_name}</h1>
+      <h2>{project.name}</h2>
+      <p>{project.description}</p>
       <div className="project-card-content">
-        <p>{project.description}</p>
+        <ul>
+          {langs.length < 1 ? (
+            <li>{project.language}</li>
+          ) : (
+            langs.map((lang) => <li key={crypto.randomUUID()}>{lang}</li>)
+          )}
+        </ul>
+        <GitHubButton link={project.clone_url} />
       </div>
     </div>
   );
