213c628ecf43849fc6d9b30b4e87c152b99c64b3
pikoseq9 <wekpat@wp.pl>
2025-08-31 20:08:20 +0200

    fix Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Logo() {
-  return <img className="logo" src="./logo.svg" alt="Logo"></img>;
+  return <img className="logo" src="./Logo.svg" alt="Logo"></img>;
 }
